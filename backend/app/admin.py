509e23629e2cf372068f72978fa5ebc1f3e4ba6a
gregpr07 <gregor.ninjaboy@gmail.com>
2019-11-09 13:23:32 +0100

    database_first

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from app.models import UserInfo, Material
 # Register your models here.
+
+admin.site.register(UserInfo)
+admin.site.register(Material)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField, JSONField
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    params = JSONField()
+
+
+""" 
+how to use:
+############
+
+from app.models import UserInfo
+from django.contrib.auth.models import User
+
+user = User.objects.get(username='martin')
+
+UserInfo.save()
+
+
+############
+ """
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    vector = ArrayField(models.IntegerField())
+    IDKparam = models.CharField(null=True, max_length=100)
