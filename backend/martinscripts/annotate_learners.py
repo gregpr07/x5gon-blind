7d9e8f007a2c04f7dd06cc6857bfded07106f121
MolanM <martin@molan.net>
2020-02-05 11:41:57 +0100

    Negative representation

--- backend/martinscripts/annotate_learners.py
+++ backend/martinscripts/annotate_learners.py
@@ -50,7 +50,22 @@ def most_important_value_topic(new_dict):
         max_mu = 0
         max_sigma = 0
         for value in new_dict[topic].keys():
-            if abs(new_dict[topic][value]['mu']) > abs(new_dict[topic][max_value]['mu']):
+            if new_dict[topic][value]['mu'] > new_dict[topic][max_value]['mu']:
+                max_value = value
+                max_mu = new_dict[topic][value]['mu']
+                max_sigma = new_dict[topic][value]['sigma']
+        max_value_dict[topic] = {'value': max_value, 'mu': max_mu, 'sigma':max_sigma}
+    return(max_value_dict)
+
+
+def most_important_value_topic_negative(new_dict):
+    max_value_dict = {}
+    for topic in new_dict.keys():
+        max_value = 0
+        max_mu = 0
+        max_sigma = 0
+        for value in new_dict[topic].keys():
+            if new_dict[topic][value]['mu'] < new_dict[topic][max_value]['mu']:
                 max_value = value
                 max_mu = new_dict[topic][value]['mu']
                 max_sigma = new_dict[topic][value]['sigma']
@@ -60,6 +75,14 @@ def most_important_value_topic(new_dict):
 def topic_summary(annotated_dict):
     return most_important_value_topic(dist_sum_per_value(annotated_dict))
 
+def topic_summary_negative(annotated_dict):
+    return most_important_value_topic_negative(dist_sum_per_value(annotated_dict))
+
 def player_summary(learner):
     summary = topic_summary(annotate_learner(learner))
+    return [list(summary.keys()), list(summary.values())]
+
+
+def player_summary_negative(learner):
+    summary = topic_summary_negative(annotate_learner(learner))
     return [list(summary.keys()), list(summary.values())]
