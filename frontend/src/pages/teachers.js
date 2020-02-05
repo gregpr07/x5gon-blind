b1a01f5e851f13ffbd667f28fa020f65cfdffadd
gregpr07 <gregor.ninjaboy@gmail.com>
2020-02-05 11:27:10 +0100

    text

--- frontend/src/pages/teachers.js
+++ frontend/src/pages/teachers.js
@@ -20,13 +20,13 @@ const Header = () => {
 				<div className="row no-gutters my-auto mt-md-0">
 					<div className="col-md-12 col-lg-6 my-auto">
 						<div className="main-content pl-1 ml-4">
-							<h1 className="text-black text-main-header">Undestand</h1>
+							<h1 className="text-black text-main-header">Understand</h1>
 							<h4 className="body-2">your students with</h4>
 							<h1 className="text-black text-main-header">
 								<b className="d-block">PYTHIA</b>
 							</h1>
 							<p className="mt-3 pt-3 text-black w-100 body-2 pb-2 pr-4 pr-md-3">
-								Understand your students' accessibility progress and monitor
+								Understand your students' accessibility preferences and monitor
 								their progress
 							</p>
 						</div>
