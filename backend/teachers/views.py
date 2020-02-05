d325134042180207333318f2eeaea2dad343e76f
MolanM <martin@molan.net>
2020-02-05 12:32:13 +0100

    blind label fix

--- backend/teachers/views.py
+++ backend/teachers/views.py
@@ -97,9 +97,11 @@ class presentPlayers(APIView):
         clusters = GM.predict(reprs).tolist()
 
         usernames = [x.user.username for x in users]
+        
+        types = [x.user.type for x in users]
 
         usersinfo = [{'x': obj[0], 'y': obj[1], 'type': obj[2], 'r': 10, 'user':obj[3]}
-                     for obj in zip(repX, repY, clusters, usernames)]
+                     for obj in zip(repX, repY, types, usernames)]
 
         formatted = [
             {
