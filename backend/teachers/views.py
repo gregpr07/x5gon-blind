ac10c504c3fadc732e47c02c0cf106d1cc39be66
MolanM <martin@molan.net>
2020-02-06 08:24:06 +0100

    Teachers

--- backend/teachers/views.py
+++ backend/teachers/views.py
@@ -98,7 +98,7 @@ class presentPlayers(APIView):
 
         usernames = [x.user.username for x in users]
         
-        types = [x.user.type for x in users]
+        types = [x.type for x in users]
 
         usersinfo = [{'x': obj[0], 'y': obj[1], 'type': obj[2], 'r': 10, 'user':obj[3]}
                      for obj in zip(repX, repY, types, usernames)]
